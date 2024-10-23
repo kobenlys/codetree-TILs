499cf9c1d1fc733b55fb9e106c71f6c1a2275cf0
영석 <149328708+kobenlys@users.noreply.github.com>
2024-10-23 15:29:04 +0900

    Title: 오목의 승패, Time: 117ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 241023/오목의 승패/concave-victory-and-defeat.java
+++ 241023/오목의 승패/concave-victory-and-defeat.java
@@ -5,32 +5,39 @@ public class Main {
     public static int[][] arr1;
 
     public static void isWin(int color, int x, int y){
-        int[] dx = {0,0,-1,1,-1,1,-1,1};
-        int[] dy = {-1,1,0,0,-1,-1,1,1};
+        int[] dx1 = {0,-1,-1,1};
+        int[] dy1 = {-1,0,-1,-1};
+        int[] dx2 = {0,1,1,-1};
+        int[] dy2 = {1,0,1,1};
 
-        for(int i=0; i<8; i++){
+        for(int i=0; i<4; i++){
             int cnt = 1;
-            int ansX = x+1;
-            int ansY = y+1;
+            int ansX = x;
+            int ansY = y;
 
             for(int j=1; j<=6; j++){
-                int nx = x + dx[i] * j;
-                int ny = y + dy[i] * j;
+                int nx = x + dx1[i] * j;
+                int ny = y + dy1[i] * j;
                 if(nx < 0 || ny < 0 || nx >=19 || ny >= 19) break;
                 if(arr1[ny][nx] != color) break;
                 cnt++;
+            }
 
-                if(cnt == 5){
-                    if(i == 6){
-                        ansX = nx + 1;
-                        ansY = ny + 1;
-                    }
+            for(int j=1; j<=6; j++){
+                int nx = x + dx2[i] * j;
+                int ny = y + dy2[i] * j;
+                if(nx < 0 || ny < 0 || nx >=19 || ny >= 19) break;
+                if(arr1[ny][nx] != color) break;
+                cnt++;
+                if(i == 3){
+                    ansX = nx;
+                    ansY = ny;
                 }
-            }   
+            }
 
             if(cnt == 5){
                 System.out.println(color);
-                System.out.println(ansY +" "+ ansX);
+                System.out.println(ansY+1 +" "+ (ansX+1));
                 System.exit(0);
             }
             
